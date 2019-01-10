43b245204e17ae2b254236a41388f9e94a73da8e
Tiago Temporin <tiaguinhocab@gmail.com>
2019-01-10 14:59:54 +0000

    feat: clear tags from document

---
+++ src/lib/remove.ts
@@ -0,0 +1,18 @@
+import { RemoveTags } from './config';
+
+/**
+ *
+ */
+export const remove = (doc: HTMLElement, tags: RemoveTags) => {
+  let selectors: string[] = [];
+  if (tags.link) selectors.push('[rel="stylesheet"]');
+  if (tags.style) selectors.push('style');
+  if (tags.script) selectors.push('script');
+
+  doc.querySelectorAll(selectors.join(',')).forEach((el: HTMLElement) => {
+    if (!el.dataset.preserve) {
+      el.remove();
+    }
+  });
+}
+
