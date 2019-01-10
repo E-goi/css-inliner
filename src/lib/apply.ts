96623a30d8a69fd437785f264df49139e8527ddd
Tiago Temporin <tiaguinhocab@gmail.com>
2019-01-10 18:35:19 +0000

    feat: validate keyword !important

--- src/lib/apply.ts
+++ src/lib/apply.ts
@@ -2,7 +2,8 @@ import { StyleMap, toObject } from './style';
 
 interface InlineElementStyle {
   element: HTMLElement;
-  preserve_style: (string | number)[];
+  important: (string | number)[];
+  preserve: (string | number)[];
 }
 
 export const apply = (doc: HTMLElement, styles: StyleMap[]): void => {
@@ -15,15 +16,27 @@ export const apply = (doc: HTMLElement, styles: StyleMap[]): void => {
         const style = el.getAttribute('style');
         element = {
           element: el,
-          preserve_style: style && toObject(style).map(style => style && style.attr || null) || []
+          important: [],
+          preserve: style && toObject(style).map(style => style && style.attr || null) || []
         };
 
         elements.push(element);
       }
 
       item.style.forEach(style => {
-        if (style && element.preserve_style.indexOf(style.attr) === -1) {
-          el.style[style.attr] = style.value;
+        if (style && element.preserve.indexOf(style.attr) === -1) {
+          // validate if style is !important
+          const isImportant = style.value.toString().indexOf('!important') !== -1;
+
+          if (element.important.indexOf(style.attr) !== -1 && !isImportant) {
+            return;
+          }
+
+          if (isImportant) {
+              element.important.push(style.attr);
+          }
+
+          el.style[style.attr] = style.value.toString().replace('!important', '');
         }
       });
     });
