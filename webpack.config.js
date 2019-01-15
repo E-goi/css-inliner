36efa8484aaa621c9210f3ed5a6ed748d2733930
Tiago Temporin <tiaguinhocab@gmail.com>
2019-01-15 12:34:00 +0000

    refactor: change webpack build file to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   output: {
     libraryTarget: 'umd',
-    filename: 'bundle.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
